0eb83ffaf13feb1d0d644da3ec9a04d6032311d9
Gabriel silva <gabriel23teotonio@gmail.com>
2022-08-21 14:46:59 -0300

    add new project in projects

--- js/scrollReveal.js
+++ js/scrollReveal.js
@@ -1,6 +1,21 @@
 
+// reveal Home
 
-ScrollReveal({ reset: true });
+
+ScrollReveal().reveal('.home-content', {
+    delay: 300,
+    distance: '50px',
+    origin: 'left'
+});
+
+ScrollReveal().reveal('.home-image', {
+    delay: 300,
+    distance: '50px',
+    origin: 'right'
+});
+
+// 
+
 ScrollReveal().reveal('.skill-1', {
     delay: 300,
     distance: '50px',
@@ -23,3 +38,31 @@ ScrollReveal().reveal('.projects', {
     distance: '50px',
     origin: 'left'
 });
+
+
+// reveal about
+
+ScrollReveal().reveal('.content-text', {
+    delay: 300,
+    distance: '50px',
+    origin: 'left'
+});
+ScrollReveal().reveal('.about-img', {
+    delay: 300,
+    distance: '50px',
+    origin: 'left'
+});
+ScrollReveal().reveal('.about-buttons', {
+    delay: 400,
+    distance: '50px',
+    origin: 'bottom'
+});
+
+
+// reveal contact
+ScrollReveal().reveal('.contact', {
+    delay: 400,
+    distance: '50px',
+    origin: 'bottom'
+});
+
